d0d64132f21af35e8f1e3ada2d2892480a6e8844
Amir Omidi <amir@aaomidi.com>
2018-10-23 15:27:15 -0400

    Cleanup

--- main.go
+++ main.go
@@ -14,28 +14,16 @@ func main() {
 		Mult: *big.NewInt(8),
 	}
 
-	//private := knapsack.PrivateKey{
-	//	Key:  []int64{2, 7, 11, 21, 42, 89, 180, 354},
-	//	Mod:  *big.NewInt(881),
-	//	Mult: *big.NewInt(588),
-	//}
-
 	cipher := knapsack.Cipher{
 		PrivateKey: private,
 	}
 
 	x := cipher.Decrypt(big.NewInt(20))
-	fmt.Println(x.BtoStr())
+	fmt.Printf("Decrypted value of 20: %s\n", x.BtoStr())
 
 	x = cipher.Decrypt(big.NewInt(29))
-	fmt.Println(x.BtoStr())
+	fmt.Printf("Decrypted value of 29: %s\n", x.BtoStr())
 
 	cipher.FindPublicKey()
-	fmt.Println(cipher.PublicKey.Key)
-
-	input := []bool{false, true, true, false, false, false, false, true}
-	binaryInput := knapsack.Binary(input)
-
-	y := cipher.Encrypt(&binaryInput)
-	fmt.Println(y)
+	fmt.Printf("Public key: %v\n", cipher.PublicKey.Key)
 }
